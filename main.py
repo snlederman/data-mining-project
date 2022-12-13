24ebea8ffa73c40b06f934c8b7fe81f470dfe04e
Samuel Lederman <snledermans@gmail.com>
2022-12-13 19:20:59 +0200

    add last changes

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ DATABASE_NAME = read_from_config('DATABASE_NAME')
 
 
 def delete_database(user, password):
+    """'delete_database' asks the client to delete the 'shufersal' database and if positive it deletes it."""
     delete_database_q = input(f"this will delete the existing '{DATABASE_NAME}' database, to proceed? [y/n]")
     if delete_database_q == 'y':
         try:
             database.delete_database(user, password, DATABASE_NAME)
             print(f'"{DATABASE_NAME}" database was deleted successfully.')
-            logging.info(f'database was deleted successfully: %s')
+            logging.info(f'"{DATABASE_NAME}" database was deleted successfully: %s')
         except pymysql.err.OperationalError:
-            print(f"Can't delete database {DATABASE_NAME}, database doesn't exist")
-            logging.warning(f"Can't delete database {DATABASE_NAME}, database doesn't exist: %s")
+            print(f"Can't delete {DATABASE_NAME} database because it doesn't exist")
+            logging.warning(f"Can't delete {DATABASE_NAME} database because it doesn't exist: %s")
             return
     elif delete_database_q == 'n':
         return
@@ -34,6 +35,7 @@ def delete_database(user, password):
 
 
 def build_shufersal_database(user, password):
+    """'build_shufersal_database' creates the 'shufersal' database."""
     try:
         database.main(user, password)
         print(f'"{DATABASE_NAME}" database was created successfully.')
@@ -44,12 +46,13 @@ def build_shufersal_database(user, password):
 
 
 def check_sql_connection(user, password):
+    """'check_sql_connection' checks for """
     try:
         connection(user, password)
-        logging.info(f'MySQL connection established: %s')
+        logging.info(f'MySQL connection established: %s', user)
         return True
     except pymysql.err.OperationalError:
-        logging.critical(f'no MySQL connection established: %s')
+        logging.critical(f'no MySQL connection established: %s', user)
         return False
 
 
